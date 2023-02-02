d6a743225ccd381650e98bead66abbae1592f66f
jb-li <79426034+jb-li@users.noreply.github.com>
2023-02-01 17:45:15 -0800

    Add code for figure 4

--- scripts/main/02_04_figure04.R
+++ scripts/main/02_04_figure04.R
@@ -2,3 +2,52 @@ source("scripts/setup/00_01_load_packages.R")
 source("scripts/setup/00_02_load_functions.R")
 source("scripts/setup/00_03_load_settings.R")
 
+historical_vpd <- read_rds(file.path(path_dropbox, 'data', 'gridmet_vpd_1979_2022_1deg_forest_cover.rds')) %>% filter(west==1)
+vpd_obs_month <- historical_vpd %>% filter(month>4, month<10) %>% group_by(year, month) %>% 
+  summarise(vpd=weighted.mean(vpd, forest, na.rm=T)) %>% ungroup()
+vpd_obs_year <- vpd_obs_month %>% group_by(year) %>% summarise(vpd=mean(vpd,na.rm=T)) %>% ungroup() 
+
+dt <- read_rds(file.path(path_dropbox, "data", "epa_station_year_filled2022.rds"))
+sc <- read_rds(file.path(path_dropbox, 'data', 'statecodes.rds'))
+dt <- left_join(dt,sc)
+
+ws <- c("California","Oregon","Washington","Nevada","Arizona","New Mexico","Utah","Idaho","Montana","Wyoming","Colorado")
+dty <- dt %>% mutate(smokePM = totalPM_mean-nonsmokePM_mean) %>% filter(state%in%ws) %>% group_by(year) %>% summarise(smokePM=mean(smokePM,na.rm=T))
+
+# load gcm data
+gcm <- read_rds(file.path(path_dropbox, 'data', 'CMIP6_west_vpd_debias_annual_2015_2100.rds'))
+gcms <- gcm %>% filter(year>=2040 & year<=2060) %>%
+  group_by(model,scenario) %>% summarise(vpd=mean(vpd_debias))
+
+
+toplot = full_join(dty, vpd_obs_year)
+
+
+pdf(file=file.path(path_github, "figures", "raw", "figure04_Figure_VPD_smoke_v2.pdf"),height=4,width=8)
+par(mfrow=c(1,2),mar=c(4,4,1,1))
+plot(toplot$vpd,log(toplot$smokePM),pch=21,axes=F,xlab="May-Oct avg VPD (kPa)",ylab = "annual smoke PM (ug/m3)",bg=alpha("grey",50),xlim=c(0.85,1.1))
+axis(1)
+att=c(0.1,0.2,0.5,1,2,3)
+axis(2,las=1,at=log(att),labels=att)
+text(toplot$vpd+0.02,log(toplot$smokePM),labels = toplot$year,cex=0.7)
+abline(lm(log(smokePM) ~ vpd,toplot),lty=2)
+
+plot(vpd_obs_year,type="l",axes=F,xlim=c(1980,2032),ylim=c(0.75,1.25),ylab="May-Oct avg VPD (kPa)")
+att=seq(1980,2020,10)
+axis(1,at=att,att)
+axis(2,las=1)
+abline(lm(vpd~year,vpd_obs_year),lty=2)
+ss <- c("ssp126","ssp245","ssp370")
+colz=met.brewer("Hiroshige",3,direction = -1)
+xloc= c(2024,2027,2030)
+w=2  #width
+for (s in 1:length(ss)) {
+  out <- gcms %>% filter(scenario==ss[s]) 
+  qq <- quantile(out$vpd,probs=c(0.1,0.25,0.5,0.75,0.9))
+  rect(xloc[s],qq[1],xloc[s]+w,qq[5],col=alpha(colz[s],0.3),border = NA)
+  rect(xloc[s],qq[2],xloc[s]+w,qq[4],col=alpha(colz[s],0.6),border = NA)
+  segments(xloc[s],qq[3],xloc[s]+w,qq[3],col=colz[s],lwd=2)
+  
+}
+
+dev.off()
